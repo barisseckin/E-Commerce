f494456f545c6333cb22a859e6248ce937b760c2
Baris <barisseckin0@gmail.com>
2022-01-27 14:20:08 +0300

    delete

--- src/main/java/com/eCommerceProject/dataAccess/abstracts/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.eCommerceProject.dataAccess.abstracts;
-
-import com.eCommerceProject.entities.concretes.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDao extends JpaRepository<User, Integer> {
-    User getById(int id);
-}
