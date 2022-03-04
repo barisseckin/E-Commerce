9776e2aa143abad9944f0fd8894ab7a80a4a75f3
Baris0 <barisseckin0@gmail.com>
2022-03-04 11:38:01 +0300

    last security

--- src/main/java/com/eCommerceProject/security/SecurityConfig.java
+++ src/main/java/com/eCommerceProject/security/SecurityConfig.java
@@ -1,4 +1,41 @@
 package com.eCommerceProject.security;
 
-public class Configuration {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${service-Admin_name}")
+    private String ADMIN_NAME;
+
+    @Value("${serive-Admin_password}")
+    private String ADMIN_PASSWORD;
+
+    @Value("${service-User_name}")
+    private String USER_NAME;
+
+    @Value("${service-User_password}")
+    private String USER_PASSWORD;
+
+    BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder)
+                .withUser(ADMIN_NAME)
+                .password(passwordEncoder.encode(ADMIN_PASSWORD))
+                .roles("ADMİN");
+
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder)
+                .withUser(USER_NAME)
+                .password(passwordEncoder.encode(USER_PASSWORD))
+                .roles("USER");
+    }
 }
--- src/test/java/com/eCommerceProject/business/concretes/CategoryManagerTest.java
+++ src/test/java/com/eCommerceProject/business/concretes/CategoryManagerTest.java
@@ -1,11 +1,22 @@
 package com.eCommerceProject.business.concretes;
 
+import com.eCommerceProject.business.abstracts.CategoryService;
+import com.eCommerceProject.dataAccess.abstracts.CategoryDao;
+import com.eCommerceProject.model.Category;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 class CategoryManagerTest {
 
-    @Test
+    @Mock
+    CategoryDao categoryDao;
+
+    @InjectMocks
+    CategoryService categoryService;
+
     void getAll() {
     }
 
@@ -15,5 +26,6 @@ class CategoryManagerTest {
 
     @Test
     void getByCategoryName() {
+
     }
 }
