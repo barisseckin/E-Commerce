644d95842483854003cbc389f8e5f9a90dc9dca1
Baris0 <barisseckin0@gmail.com>
2022-05-06 03:02:06 +0300

    ProductManagerTest.class update

--- src/test/java/com/eCommerceProject/business/concretes/ProductManagerTest.java
+++ src/test/java/com/eCommerceProject/business/concretes/ProductManagerTest.java
@@ -2,6 +2,7 @@ package com.eCommerceProject.business.concretes;
 
 import com.eCommerceProject.dataAccess.abstracts.ProductDao;
 import com.eCommerceProject.dto.createDto.ProductCreateDto;
+import com.eCommerceProject.dto.viewDto.ProductViewDto;
 import com.eCommerceProject.model.Product;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,6 +10,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -34,6 +38,20 @@ class ProductManagerTest {
 
     @Test
     void testGetAll() {
+        Product product = new Product();
+        product.setId(1);
+        product.setProductName("Test-Name");
+        product.setProductBrand("Test-Brand");
+        product.setProductDetails("Test-Details");
+        product.setProductImageUrl("Test-Url");
+        product.setStock(10);
+        product.setProductPrice(1000);
+
+        when(productDao.findAll()).thenReturn(Collections.singletonList(product));
+        List<Product> productList = productManager.getAll();
+
+        assertEquals(productList.size(), 1);
+
     }
 
     @Test
@@ -74,6 +92,7 @@ class ProductManagerTest {
         String productBrand = "Test-Brand";
         productManager.getByproductBrand(productBrand);
         verify(productDao).getByproductBrand(productBrand);
+
     }
 
     @Test
@@ -87,9 +106,26 @@ class ProductManagerTest {
 
     @Test
     void testSlice() {
+
     }
 
     @Test
+    @DisplayName("product get dto test")
     void testGetDto() {
+        Product product = new Product();
+        product.setId(1);
+        product.setProductName("Test-Name");
+        product.setProductBrand("Test-Brand");
+        product.setProductDetails("Test-Details");
+        product.setProductImageUrl("Test-Url");
+        product.setStock(10);
+        product.setProductPrice(1000);
+
+        when(productDao.findAll()).thenReturn(Collections.singletonList(product));
+        List<ProductViewDto> productViewDtos = productManager.getDto();
+
+        assertEquals(productViewDtos.size(), 1);
+
+
     }
 }
